7d5f56ce153ffaa4cfb9b6373b7e548ce2f969f3
Pasan Madhuranga <pasan3529@gmail.com>
2023-12-29 21:56:27 +0530

    solve 189. Rotate Array

---
+++ Top Interview 150/189_Rotate_Array.py
@@ -0,0 +1,44 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        
+
+# Another solution found in the discussion section
+        
+# class Solution {
+#     public void rotate(int[] nums, int k) {
+#     k %= nums.length;
+#     reverse(nums, 0, nums.length - 1);
+#     reverse(nums, 0, k - 1);
+#     reverse(nums, k, nums.length - 1);
+#     }
+
+#     public void reverse(int[] nums, int start, int end) {
+#         while (start < end) {
+#             int temp = nums[start];
+#             nums[start] = nums[end];
+#             nums[end] = temp;
+#             start++;
+#             end--;
+#         }
+#     }
+# }
+
+# Explanation:
+# let a= [1,2,3,4,5,6,7]
+# k = 3.
+
+# we have to first reverse the whole array by swapping first element with the last one and so on..
+# you will get[7,6,5,4,3,2,1]
+
+# reverse the elements from 0 to k-1
+# reverse the elements 7,6,5
+# you will get [5,6,7,4,3,2,1]
+
+# reverse the elements from k to n-1
+# reverse the elements 4,3,2,1
+# you will get[5,6,7,1,2,3,4]
