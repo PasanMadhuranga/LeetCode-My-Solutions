ecbd46dd84756a9d528085d3c5ae19323ccb551f
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-01-12 06:02:22 +0530

    Update 380. Insert Delete GetRandom O(1).java

--- Top Interview 150/380. Insert Delete GetRandom O(1).java
+++ Top Interview 150/380. Insert Delete GetRandom O(1).java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
-import java.util.Random;   
+import java.util.Random;
 
 class RandomizedSet {
     ArrayList<Integer> randomSet;
 
     public RandomizedSet() {
-        randomSet =  new ArrayList<Integer>();
+        randomSet = new ArrayList<Integer>();
     }
-    
+
     public boolean insert(int val) {
-        if (randomSet.contains(val)) return false;
+        if (randomSet.contains(val))
+            return false;
         randomSet.add(val);
         return true;
 
     }
-    
+
     public boolean remove(int val) {
         if (randomSet.contains(val)) {
-            randomSet.remove(Integer.valueOf(val)); 
+            randomSet.remove(Integer.valueOf(val));
             return true;
         }
         return false;
     }
-    
+
     public int getRandom() {
         Random random = new Random();
         return randomSet.get(random.nextInt(randomSet.size()));
@@ -36,3 +37,56 @@ class RandomizedSet {
  * boolean param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
  */
+
+// Another solution found in discussion section
+// Using HashMap and ArrayList
+// public class RandomizedSet {
+// ArrayList<Integer> nums;
+// HashMap<Integer, Integer> locs;
+// java.util.Random rand = new java.util.Random();
+
+// /** Initialize your data structure here. */
+// public RandomizedSet() {
+// nums = new ArrayList<Integer>();
+// locs = new HashMap<Integer, Integer>();
+// }
+
+// /**
+// * Inserts a value to the set. Returns true if the set did not already contain
+// * the specified element.
+// */
+// public boolean insert(int val) {
+// boolean contain = locs.containsKey(val);
+// if (contain)
+// return false;
+// locs.put(val, nums.size());
+// nums.add(val);
+// return true;
+// }
+
+// /**
+// * Removes a value from the set. Returns true if the set contained the
+// specified
+// * element.
+// */
+// public boolean remove(int val) {
+// boolean contain = locs.containsKey(val);
+// if (!contain)
+// return false;
+// int loc = locs.get(val);
+// if (loc < nums.size() - 1) { // not the last one than swap the last one with
+// this val
+// int lastone = nums.get(nums.size() - 1);
+// nums.set(loc, lastone);
+// locs.put(lastone, loc);
+// }
+// locs.remove(val);
+// nums.remove(nums.size() - 1);
+// return true;
+// }
+
+// /** Get a random element from the set. */
+// public int getRandom() {
+// return nums.get(rand.nextInt(nums.size()));
+// }
+// }
